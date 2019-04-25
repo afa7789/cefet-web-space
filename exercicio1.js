55593f510348faf57e57617aa51dd0ac668362c5
Malakyas <afa7789@gmail.com>
2019-04-25 12:20:16 -0300

    feito na aula parte1
    aasad

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,21 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let buttonExerciseElArray = document.querySelectorAll('.botao-expandir-retrair')
+
+for ( let  buttonExerciseEl of  buttonExerciseElArray){
+	buttonExerciseEl.addEventListener('click',function(e) {
+		let button=e.target
+		let buttonParentEl = button.parentElement
+		let contentButtonExpanded = "-"
+		let contentButtonMinimized = "+"
+			
+		if( buttonParentEl.classList.contains("expandido")){
+			buttonParentEl.classList.remove("expandido")
+			button.innerHTML = contentButtonExpanded	
+		}else{
+			buttonParentEl.classList.add("expandido")
+			button.innerHTML=contentButtonMinimized	
+		}
+	});
+}
--- exercicio2.js
+++ exercicio2.js
@@ -1,5 +1,6 @@
 // Faça o exercício da galeria de imagens aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+// Este arquivo está incluído no arquivo HTML
+'use strict';
 
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
   todasAsImagens = [
@@ -9,3 +10,30 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+const buttonsNavigateEl = document.querySelectorAll(".controle")
+
+for (let b of buttonsNavigateEl){
+	b.addEventListener('click',function(e){
+		let butaoClicado=e.target
+		let div=butaoClicado.parentElement	
+		let Galeria= div.querySelector("#slide")
+		let TVI = todasAsImagens.length //tamanhoVetorImagem
+		let i = 0
+		//i=(i+1)%todasAsImagens.length
+		for( let imagem of todasAsImagens){		
+			if(Galeria.src === imagem){
+				if(butaoClicado.id == "antecessor"){
+					Galeria.src=todasAsImagens[((i-1)+TVI)%TVI]
+				}
+				else{
+					Galeria.src=todasAsImagens[(i+1)%TVI]
+				}
+			}
+			i++
+		}	
+	});
+}
+
+
+
